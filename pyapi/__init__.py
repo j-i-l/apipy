6a6b955b5b8500d4d24859903077487230d60bfe
Jonas I Liechti <jonas.i.liechti@gmail.com>
2014-03-23 21:26:29 +0100

    micro

--- pyapi/__init__.py
+++ pyapi/__init__.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
-
-# <codecell>
-
 #from .ApiRequests import Request
 #from .ApiRequestsPrivate import RequestPrivate
 from .ApiAccountStructure import AccountStructure
